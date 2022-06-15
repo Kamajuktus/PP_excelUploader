767ff88aa38fd5406cd7b382f3ffae5a4570c742
DarkBlueBox <80337691+DarkBlueBox@users.noreply.github.com>
2022-06-15 14:21:04 +0600

    Update forms.py

--- PP_1/excelUploader/forms.py
+++ PP_1/excelUploader/forms.py
@@ -1,8 +1,17 @@
 
+
 # forms.py
 from django import forms
+from .models import UploadExcel
+from django.forms import ClearableFileInput
 
 
-class FileFieldForm(forms.Form):
-    file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={
-        'multiple': True}))
+class FileFieldForm(forms.ModelForm):
+    class Meta:
+        model = UploadExcel
+        fields = ['file_field']
+        widgets = {
+            'file_field': ClearableFileInput(attrs={'multiple': True}),
+        }
+    # file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={
+    #     'multiple': True}))
