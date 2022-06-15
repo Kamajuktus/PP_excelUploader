5f7e1b0e997cdd2a43b636ec5799ee2b5ac7a3f9
DarkBlueBox <80337691+DarkBlueBox@users.noreply.github.com>
2022-06-15 14:21:50 +0600

    Update models.py

--- PP_1/excelUploader/models.py
+++ PP_1/excelUploader/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django import forms
+
+
 class Employee(models.Model):
     employee_name = models.CharField(max_length=50)
 
@@ -8,6 +9,7 @@ class Employee(models.Model):
 
     objects = models.Manager()
 
+
 class ACS(models.Model):
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE, null=True)
     company = models.CharField(max_length=50)
@@ -15,7 +17,7 @@ class ACS(models.Model):
     direction = models.CharField(max_length=5)
     door = models.CharField(max_length=50)
 
-    # def __str__(self):
-    #     return self.employee_id
-
     objects = models.Manager()
+
+class UploadExcel(models.Model):
+    file_field = models.FileField(upload_to='media/', blank=True, null=True)
